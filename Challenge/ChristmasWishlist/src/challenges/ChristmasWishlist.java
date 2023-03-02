013c863101c5f151bb79929d9ed8f0b144b4cfcc
iuli <delenaiulia@gmail.com>
2023-03-02 18:04:34 +0200

    Challenge

--- Challenge/ChristmasWishlist/src/challenges/ChristmasWishlist.java
+++ Challenge/ChristmasWishlist/src/challenges/ChristmasWishlist.java
@@ -6,10 +6,6 @@ import java.util.List;
 public class ChristmasWishlist {
     public static void main(String[] args) {
         List<String> christmasWishlist = Arrays.asList("Money", "Sweets", "Puppy", "Socks");
-        /*christmasWishlist.add("Money");
-        christmasWishlist.add("Sweets");
-        christmasWishlist.add("Puppy");
-        christmasWishlist.add("Socks");*/
         System.out.println(christmasWishlist);
     }
 }
